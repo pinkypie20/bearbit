ebcb1ad50e59e1a5beaab1466d859220771323b1
pinkypie20 <163538466+pinkypie20@users.noreply.github.com>
2025-11-06 21:18:54 +0700

    Update cover-next.user.js

--- cover-next.user.js
+++ cover-next.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cover Next (Bearbit)
 // @namespace    http://tampermonkey.net/
-// @version      2.25.3
+// @version      2.25.4
 // @description  tools that will help you see new items and make it easier for you to use the website to load bittorrent.
 // @author       NEKO
 // @icon         https://i.imgur.com/qTvfJSr.png
@@ -87,5 +87,6 @@
 // @connect      www.imagenetz.de
 // @connect      *
 // ==/UserScript==
+
 
 
